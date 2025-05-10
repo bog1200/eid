b53be5f48f2c484b47a570ea2d5cb71cd940924a
bog1200 <26150024+bog1200@users.noreply.github.com>
2025-05-10 23:33:21 +0300

    Orange: blockchain 6 nodes script fix

--- CountryNodes/orange/blockchain/copyall.sh
+++ CountryNodes/orange/blockchain/copyall.sh
@@ -3,6 +3,7 @@
 # Get sorted lists of input and output folders
 input_folders=(networkFiles/keys/0x*/)
 output_folders=(nodes/Node-*/data/)
+genesis_folders=(nodes/Node-*/)
 
 # Check if input and output folder counts match
 if [ "${#input_folders[@]}" -ne "${#output_folders[@]}" ]; then
@@ -19,11 +20,13 @@ for i in "${!input_folders[@]}"; do
   echo "Copying keys from $input to $output"
   cp -r "$input"* "$output"
 done
-
+echo "✅ All keys copied."
 # Copy genesis.json to each Node-* directory
-for node_dir in Node-*; do
-  echo "Copying genesis.json to $node_dir/"
-  cp networkFiles/genesis.json "$node_dir/"
+for node_dir in nodes/Node-*; do
+  if [ -d "$node_dir" ]; then
+    echo "Copying genesis.json to $node_dir"
+    cp networkFiles/genesis.json "$node_dir/"
+  fi
 done
 
-echo "✅ All keys and genesis files copied."
+echo "✅Genesis files copied."
