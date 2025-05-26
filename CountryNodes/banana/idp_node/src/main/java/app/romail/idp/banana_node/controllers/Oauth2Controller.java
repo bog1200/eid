61bb5a442b0eb6c553b2b4e2f24f1941c6185a41
bog1200 <26150024+bog1200@users.noreply.github.com>
2025-05-26 13:41:49 +0300

    Banana: userinfo test 2

--- CountryNodes/banana/idp_node/src/main/java/app/romail/idp/banana_node/controllers/Oauth2Controller.java
+++ CountryNodes/banana/idp_node/src/main/java/app/romail/idp/banana_node/controllers/Oauth2Controller.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,19 +18,15 @@ public class Oauth2Controller {
 
 
     @GetMapping("/oauth2/userinfo")
-    public ResponseEntity<Map<String, Object>> userinfo(Authentication authentication) {
-        if (authentication == null || !authentication.isAuthenticated()) {
+    public ResponseEntity<Map<String, Object>> userinfo(Principal principal) {
+        if (!(principal instanceof FederatedUser user)) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
-
-        Object principal = authentication.getPrincipal();
         Map<String, Object> claims = new HashMap<>();
 
-        if (principal instanceof FederatedUser user) {
-            claims.put("sub", user.getSubject());
-            claims.put("name", user.getAttributes().get("name"));
-            claims.put("email", user.getAttributes().get("email"));
-        }
+        claims.put("sub", user.getSubject());
+        claims.put("name", user.getAttributes().get("name"));
+        claims.put("email", user.getAttributes().get("email"));
 
         return ResponseEntity.ok(claims);
     }
